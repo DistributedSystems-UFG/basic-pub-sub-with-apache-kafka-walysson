a508acc6c902e0fae22f21b35d94511fde9d8d93
Walysson Paiva <walyssonpaiva@outlook.com>
2022-12-03 14:15:27 -0300

    feat: generate messages in many topics

--- producer.py
+++ producer.py
@@ -2,16 +2,18 @@ from kafka import KafkaProducer
 from const import *
 import sys
 
-try:
-    topic = sys.argv[1]
-except:
-    print ('Usage: python3 producer <topic_name>')
-    exit(1)
+
+print("Starting producer")
+print("To exit press CTRL+C or type exit as topic name")
     
 producer = KafkaProducer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
-for i in range(100):
-    msg = 'My ' + str(i) + 'st message for topic ' + topic
-    print ('Sending message: ' + msg)
-    producer.send(topic, value=msg.encode())
+
+while True:
+    input_topic = input('Enter topic name: ')
+    if input_topic == 'exit':
+        break
+    input_msg = input('Enter message: ')
+    producer.send(input_topic, value=input_msg.encode())
+    print ('Message sent')
 
 producer.flush()
